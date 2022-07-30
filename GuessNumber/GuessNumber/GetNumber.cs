fcdd6b210e44636fe7f5ff0dfbc93e762450dc1e
Mohammad Qasim Bigzad <qbigzad@gmail.com>
2022-07-30 09:45:07 +0430

    Guess Number

---
+++ GuessNumber/GuessNumber/GetNumber.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessNumber
+{
+    public class GetNumber
+    {
+        int wholeNums = new Random().Next(20);
+        bool keepPlaying = true;
+        int guessNum = 0;
+        int guessCount = 0;
+
+        public void Play()
+        {
+            Console.WriteLine("Let's Play! 'Guess the Number'!");
+            Console.WriteLine("I am thinking of a number between 0 and 20.");
+            Console.WriteLine("Enter your guess, or -1 to quit the game.");
+
+
+            do
+            {
+                Console.WriteLine("What's your guess?");
+                string theInput = Console.ReadLine();
+                bool result = Int32.TryParse(theInput, out guessNum);
+                if (!result)
+                {
+                    Console.WriteLine("Hmmm, that does not look like a number, Try again.");
+                }
+                else
+                {
+                    if (guessNum == -1)
+                    {
+                        Console.WriteLine($"Oh well. I was thinking of {wholeNums}");
+                        keepPlaying = false;
+                    }
+                    else
+                    {
+                        guessCount++;
+                        if(guessNum == wholeNums)
+                        {
+                            Console.WriteLine($"You got it in {guessCount} guesses!!");
+                            keepPlaying = false;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Nope, {0} than that.", guessNum < wholeNums ? "higher" : "lower");
+                        }
+
+                    }
+                }
+
+            } while (keepPlaying);
+        }
+    }
+}
---
+++ GuessNumber/GuessNumber/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using GuessNumber;
+
+GetNumber gs = new GetNumber();
+gs.Play();
+Console.Read();
